e0f8e443857731c71884b97aa4adaf6c7fe4f3a0
Florian Krauskopf <florian.kr@r3v0luti0n.com>
2013-08-04 11:19:52 +0200

    rewrite #1 using hist2 format

--- cloudzec/cloudzec_cli.py
+++ cloudzec/cloudzec_cli.py
@@ -27,27 +27,6 @@ def showHelp():
     print('Possible arguments:\n')
     print('  help')
     print('   -> Shows this help\n')
-    #print('  put $file')
-    #print('   -> Upload $file to server\n')
-    #print('  get $file')
-    #print('   -> Download $file from server\n')
-    #print('  history')
-    #print('   -> Get history from server\n')
-    #print('  update')
-    #print('   -> Sync with server (nothing is removed, only downloaded)\n')
-    #print('  serverinit')
-    #print('   -> Setup server if not already done)\n')
-
-
-#def ask(typ, question):
-#    from gi.repository import Notify
-#    Notify.init('CloudZec')
-#    # optionally set an icon as the last argument
-#    n = Notify.Notification.new('CloudZec', 'Hm, CloudZec wants to know something...', None)
-#    n.show()
-#    #n.close()
-#    Notify.uninit()
-#    return input(question)
 
 
 def debug(text):
@@ -59,9 +38,6 @@ def debug(text):
 
 def main():
     ## Data
-    home = os.path.expanduser('~') #os.getenv('HOME') There is no $HOME on Windows
-    confFolder = os.path.join(home, '.cloudzec')
-    confFile = os.path.join(confFolder, 'cloudzec.conf')
     c = cloud.CloudZec(debug=DEBUG)
     #cloud = Cloud(username='florian', password='~/.ssh/id_rsa', port=1337, fingerprint='5A5A3BDC', serverpath='/home/florian/cloudzec', genKeyFile=True, debug=True)
 
@@ -69,13 +45,14 @@ def main():
     args = sys.argv[1:]
     if 'help' in args or len(args) == 0:
         showHelp()
-        return 0
+    elif args[0] == 'sync':     # Sync with server (due to the way how CloudZec works it is not possible to just sync the client to the server or the other way round!)
+        c.sync()
     #elif 'serverinit' == args[0]:       # Initialise server, setup files, pubkeys, history and allocation
     #    serverinit(conf, gpgHandler)
     #elif 'autoupload' == args[0]:       # Autoupload - watch for changes in CloudZec and sync them with the server (upload only)
     #    autoupload(conf, gpgHandler)
-    elif args[0] == 'sync':             # Sync with the server
-        c.sync()
+    #elif args[0] == 'sync':             # Sync with the server
+    #    c.sync()
     else:
         showHelp()
     ## Return
